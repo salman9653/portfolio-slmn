8eaf913d8aa2ef4aa0ea45c1e9c1fb624c92a098
Salman <salman9915189734@gmail.com>
2022-10-30 16:13:52 +0530

    added skill component in skills section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {motion} from 'framer-motion';
 
-type Props = {}
+type Props = {
+    directionLeft?:boolean
+}
 
-const Skill = (props: Props) => {
+const Skill = ({directionLeft}: Props) => {
   return (
-    <div>Skill</div>
+    <div className='group relative flex cursor-pointer'>
+        <motion.img
+            src="https://cdn.sanity.io/images/xcmw07kx/production/d8c8025b1695a3f14f849b99afc71d917ef40813-480x480.png"
+            initial={{ x: directionLeft ? -200 : 200 , opacity :0 }}
+            transition={{duration:1}}
+            whileInView={{ opacity: 1, x: 0 }}
+            className="h-16 w-16 rounded-full border border-gray-600 object-cover filter group-hover:grayscale transition duration-300 ease-in-out"
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 z-0 rounded-full'>
+            <div className='flex items-center justify-center h-full'>
+                <p className='text-xl font-bold text-gray-800 opacity-100'>75%</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -18,7 +18,7 @@ const Skills = (props: Props) => {
     </h3>
     <h3 className='absolute top-32 uppercase tracking-[3px] text-sm text-gray-500'>Hover over a skill for current Proficiency</h3>
 
-    <div className='grid grid-cols-4 gap-5'>
+    <div className='grid grid-cols-4 gap-4'>
         <Skill /><Skill /><Skill /><Skill />
         <Skill /><Skill /><Skill /><Skill />
         <Skill /><Skill /><Skill /><Skill />
