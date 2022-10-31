2ea3629d30aa5ab04dbef3c5418ab6263df48094
Salman <salman9915189734@gmail.com>
2022-10-31 23:03:57 +0530

    custom scroll bar added

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,7 +16,7 @@ const Experience = (props: Props) => {
         <h3 className='absolute top-20 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-xl font-semibold'>
         Experience
         </h3>
-       <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+       <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin'>
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,8 +16,32 @@ const Projects = (props: Props) => {
         Projects
     </h3>
 
-    <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+    <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin'>
         {/* {Projects.map((project,i )=> ( */}
+            <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-14 md:p-44  h-screen'>
+                <motion.img
+                    initial={{ y:-150, opacity: 0 }}
+                    whileInView={{ y: 0, opacity : 1 }}
+                    transition={{ duration : 1 }}
+                    viewport={{once:true}}
+                    src="https://images.unsplash.com/photo-1481487196290-c152efe083f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=962&q=80"
+                    className='w-auto md:w-72'
+                    alt="" 
+                />
+                <div className='space-y-6 px-0 md:px-10 max-w-6xl'>
+                    <h4 className='text-2xl font-semibold text-center'>
+                        <span className='underline decoration-[#F7AB0A]/50'>Project 1 of 5 :</span> Music App
+                    </h4>
+                    <p className='text-lg text-center md:text-left'>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Provident error, placeat quia a nemo itaque ab voluptatibus
+                        architecto pariatur tempore cum possimus dolor eaque molestias.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Provident error, placeat quia a nemo itaque ab voluptatibus
+                    </p>
+                </div>
+            </div>
+
             <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-14 md:p-44  h-screen'>
                 <motion.img
                     initial={{ y:-200, opacity: 0 }}
@@ -41,6 +65,7 @@ const Projects = (props: Props) => {
                     </p>
                 </div>
             </div>
+            
         {/* ))} */}
     </div>
 
